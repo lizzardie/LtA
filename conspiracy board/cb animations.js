f3be2ac18eb1d99533eb5fdeca3460d52d2c66bf
lizzardie <lizzard.dh@gmail.com>
2025-04-29 16:49:32 -0700

    added dragbox to everything, pls work pls pls pls

--- conspiracy board/cb animations.js
+++ conspiracy board/cb animations.js
@@ -2,5 +2,7 @@ src="https://cdn.jsdelivr.net/npm/animejs/lib/anime.iife.min.js"
 const { animate } = anime;
 const draggable = createDraggable();
 
-anime.createDraggable('.artmini');
+anime.createDraggable('.dragbox', {
+    container: '.gridcontainer',
+});
 
