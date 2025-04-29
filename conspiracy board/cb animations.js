fe9e6c275fe2f72acd727a614dee75b30df5ea09
lizzardie <lizzard.dh@gmail.com>
2025-04-29 16:29:39 -0700

    pls work

--- conspiracy board/cb animations.js
+++ conspiracy board/cb animations.js
@@ -1,10 +1,6 @@
 src="https://cdn.jsdelivr.net/npm/animejs/lib/anime.iife.min.js"
 const { animate } = anime;
-anime.animate();
-anime.createTimeline();
+const draggable = createDraggable();
+
 anime.createDraggable('.artmini');
 
-anime({
-    targets: '.artmini',
-    translateX: 200
-})
