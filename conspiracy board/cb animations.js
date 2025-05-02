1b2bab71b2a9a36d73821ebd3f46a2d3006eea35
lizzardie <lizzard.dh@gmail.com>
2025-05-01 21:56:42 -0700

    added width and height to dice container, updated naming conventions for names with multiple words

--- conspiracy board/cb animations.js
+++ conspiracy board/cb animations.js
@@ -2,7 +2,7 @@ src="https://cdn.jsdelivr.net/npm/animejs/lib/anime.iife.min.js"
 const { animate } = anime;
 const draggable = createDraggable();
 
-anime.createDraggable(".dragbox", {
-    container: ".entireboard",
+anime.createDraggable(".drag-box", {
+    container: ".entire-board",
 });
 
