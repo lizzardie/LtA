72cfd290ec6860976d89501014ea1cd74943b34f
lizzardie <lizzard.dh@gmail.com>
2025-05-08 15:18:17 -0700

    updated draggable js again, changed textarea dimensions

--- conspiracy board/cb animations.js
+++ conspiracy board/cb animations.js
@@ -1,9 +1,14 @@
-import {animate, draggable} from 'anime.esm.js';
-const { animate } = anime;
-const draggable = createDraggable();
+import {animate, utils, createDraggable} from 'anime.esm.js';
 
 createDraggable({
     targets: ".drag-box",
     container: ".entire-board",
+    x: { snap: 32 },            
+    y: { snap: 32 },              
+    containerPadding: 10,
+    releaseStiffness: 40,
+    onGrab: () => {},
+    onDrag: () => {},
+    onRelease: () => {},
 });
 
