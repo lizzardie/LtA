19d10d204344403ae65a353824cd2fe7d2d83bc3
lizzardie <lizzard.dh@gmail.com>
2025-05-21 17:11:04 -0700

    IT WORKS :D all the dice work :))))

--- main.js
+++ main.js
@@ -5,29 +5,11 @@ var d10Total = 0;
 var d12Total = 0;
 var d20Total = 0;
 
-
 var allDice = document.querySelector(".dice-container");
-var numberChange = allDice.querySelectorAll(".numbers"); /* i can get rid of this line p sure, check when im on wifi */
-
-
 
 function rollDice(){
-
     var diceToRoll = allDice.querySelectorAll(".d20-body, .d12-body, .d10-body, .d8-body, .d6, .d4");
     /* note to future self for when i forget: random with just * 4 would return a number between 0 and 3, so the plus 1 bumps it up from 0 to 1  */
-
-    d4Total = Math.floor(Math.random() * 4 + 1);
-    d6Total = Math.floor(Math.random() * 6 + 1);
-    d8Total = Math.floor(Math.random() * 8 + 1);
-    d10Total = Math.floor(Math.random() * 10 + 1);
-    d12Total = Math.floor(Math.random() * 12 + 1);
-    d20Total = Math.floor(Math.random() * 20 + 1);
-    
-    document.querySelector(".d4 .numbers").textContent = d4Total;
-    document.querySelector(".d6 .numbers").textContent = d6Total; /* the space before .numbers is necessary! */
-    document.querySelector(".d8-body .numbers").textContent = d8Total;
-    document.querySelector(".d10-body .numbers").textContent = d10Total;
-    document.querySelector(".d12-body .numbers").textContent = d12Total;
     
     diceToRoll.forEach((dieToRoll) => {
         if (dieToRoll.className.includes("d20-body")) {
@@ -38,47 +20,24 @@ function rollDice(){
             d12Total = Math.floor(Math.random() * 12 + 1);
             dieToRoll.querySelector(".d12Numbers").textContent = d12Total;
         }
-
-
-    })
-    
-};
-
-
-/*
-
-numberChange.forEach((diceToRoll) => {
-        if(diceToRoll.className == "d20-body"){
-            diceToRoll.textContent = d20Total;
+        if (dieToRoll.className.includes("d10-body")){
+            d10Total = Math.floor(Math.random() * 10 + 1);
+            dieToRoll.querySelector(".d10Numbers").textContent = d10Total;
+        }
+        if (dieToRoll.className.includes("d8-body")){
+            d8Total = Math.floor(Math.random() * 8 + 1);
+            dieToRoll.querySelector(".d8Numbers").textContent = d8Total;
+        }
+        if (dieToRoll.className.includes("d6")){
+            d6Total = Math.floor(Math.random() * 6 + 1);
+            dieToRoll.querySelector(".d6Numbers").textContent = d6Total;
+        }
+        if (dieToRoll.className.includes("d4")){
+            d4Total = Math.floor(Math.random() * 4 + 1);
+            dieToRoll.querySelector(".d4Numbers").textContent = d4Total;
         }
     })
-
-(diceToRoll < ".d20-body").textContent = d20Total;
-
-
-var d20ToRoll = allDice.querySelectorAll(".d20-body .numbers");
-
-for (let i = 0; i < d20ToRoll.length; i++){
-        d20ToRoll[i].textContent = d20Total;
-    }
-
-
-
-for (let i = 0; i < numberChange.length; i++){
-        numberChange[i].textContent = d20Total;   ik this is wrong anyways cuz it would be turning all the numbers into d20s but why wont the rollDice function read for loops or for each functions????? 
-    }
-
-numberChange.array.forEach((diceToRoll) => {
-        document.querySelectorAll(".d20-body .numbers").textContent = d20Total;
-    })
-
-add the rest of the dice in same format to this const if this method works
-
-     use querySelectorAll to be able to roll multiple of the same die at once
-    and to maybe make the above stuff more efficient by not having to write one
-    for every kind of die and just letting it find which one through its parent class */ 
-
-/* how to assign these vars now to the divs,, */
+};
 
 function d4Spawn(){
     d4Div = document.createElement("div");
@@ -168,7 +127,57 @@ function d20Spawn(){
     d20BodyDiv.append(d20FaceDiv);
 }
 
-/*
+
+
+/* OLD STUFFS
+
+  d4Total = Math.floor(Math.random() * 4 + 1);
+    d6Total = Math.floor(Math.random() * 6 + 1);
+    d8Total = Math.floor(Math.random() * 8 + 1);
+    d10Total = Math.floor(Math.random() * 10 + 1);
+    d12Total = Math.floor(Math.random() * 12 + 1);
+    d20Total = Math.floor(Math.random() * 20 + 1);
+    
+    document.querySelector(".d4 .numbers").textContent = d4Total;
+    document.querySelector(".d6 .numbers").textContent = d6Total;
+    document.querySelector(".d8-body .numbers").textContent = d8Total;
+    document.querySelector(".d10-body .numbers").textContent = d10Total;
+    document.querySelector(".d12-body .numbers").textContent = d12Total;
+
+
+numberChange.forEach((diceToRoll) => {
+        if(diceToRoll.className == "d20-body"){
+            diceToRoll.textContent = d20Total;
+        }
+    })
+
+(diceToRoll < ".d20-body").textContent = d20Total;
+
+
+var d20ToRoll = allDice.querySelectorAll(".d20-body .numbers");
+
+for (let i = 0; i < d20ToRoll.length; i++){
+        d20ToRoll[i].textContent = d20Total;
+    }
+
+
+
+for (let i = 0; i < numberChange.length; i++){
+        numberChange[i].textContent = d20Total;   ik this is wrong anyways cuz it would be turning all the numbers into d20s but why wont the rollDice function read for loops or for each functions????? 
+    }
+
+numberChange.array.forEach((diceToRoll) => {
+        document.querySelectorAll(".d20-body .numbers").textContent = d20Total;
+    })
+
+add the rest of the dice in same format to this const if this method works
+
+     use querySelectorAll to be able to roll multiple of the same die at once
+    and to maybe make the above stuff more efficient by not having to write one
+    for every kind of die and just letting it find which one through its parent class 
+
+
+
 button.addEventListener("click", function d20Spawn(){
      let targ = document.querySelector(".dice-container");     <-- is ".target" the parent div location?
      let newItem = document.createElement("div");      <-- naming and createElementing a new div i understand
