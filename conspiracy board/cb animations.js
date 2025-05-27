c042eef643ba199c3b0a934fa323a7b19b647193
lizzardie <lizzard.dh@gmail.com>
2025-05-27 16:15:11 -0700

    added id dragbox to put position fixed on it, added potential draggable code with console.log to test

--- conspiracy board/cb animations.js
+++ conspiracy board/cb animations.js
@@ -1,5 +1,37 @@
+let startX = 0, startY = 0, newX = 0, newY = 0;
 
+var dragbox = document.getElementById('drag-box');
 
+dragbox.addEventListener('mousedown', mouseDown);
+
+function mouseDown(e){
+  startX = e.clientX;
+  startY = e.clientY;
+
+  document.addEventListener('mousemove', mouseMove);
+  document.addEventListener('mouseup', mouseUp);
+}
+
+function mouseMove(e){
+  newX = startX - e.clientX;
+  newY = startY = e.clientY;
+
+  startX = e.clientX;
+  startY = e.clientY;
+
+  dragbox.style.top = startY + 'px';
+  dragbox.style.left = startX + 'px';
+
+  console.log({newX, newY});
+  console.log({startX, startY});
+
+}
+
+function mouseUp(e){
+
+}
+
+/*
 import {createDraggable} from 'anime.esm.js';
 
 const $dragbox = document.querySelectorAll('.drag-box')
@@ -12,7 +44,7 @@ createDraggable($dragbox, {
 });
 
 
-/* reg js
+ reg js
 $(document).ready(function() {
   $(".dragBox").draggable();
 });
